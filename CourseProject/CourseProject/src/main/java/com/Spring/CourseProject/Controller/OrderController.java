008aa9347acb264c6be3d2aa094180328d582931
nabil <nabil.nader00@gmail.com>
2025-03-30 16:39:13 +0200

    fix changes

--- CourseProject/CourseProject/src/main/java/com/Spring/CourseProject/Controller/OrderController.java
+++ CourseProject/CourseProject/src/main/java/com/Spring/CourseProject/Controller/OrderController.java
@@ -3,10 +3,10 @@ package com.Spring.CourseProject.Controller;
 import com.Spring.CourseProject.Dto.OrderDto;
 import com.Spring.CourseProject.Entity.Order;
 
-import com.Spring.CourseProject.Entity.Sale;
 import com.Spring.CourseProject.Services.OrderService;
 
 import com.Spring.CourseProject.Services.SaleService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import org.springframework.web.bind.annotation.*;
@@ -14,12 +14,12 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
+@RequiredArgsConstructor
 public class OrderController {
 
-    @Autowired
-    OrderService orderservice;
-    @Autowired
-    private SaleService saleService;
+
+   private final OrderService orderservice;
+    private final SaleService saleService;
     @GetMapping("/make-order")
     public Order makeOrder(@RequestBody  List<OrderDto>orders){
         return saleService.makeOrder(orders);
